485354da36f11f7e82576e4034844d35f3dec18c
Shivelight <shivelight@tuta.io>
2023-08-24 16:53:47 +0700

    Update device.mk LOCAL_PATH

--- device.mk
+++ device.mk
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/Infinix-X6833B
+LOCAL_PATH := device/infinix/X6833B
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
