679e577da66dfa6301ec91b52270117588228c82
Shivelight <shivelight@tuta.io>
2023-08-24 22:24:31 +0700

    Fix DEVICE_PATH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X6833B
+DEVICE_PATH := device/infinix/X6833B
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -62,8 +62,8 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-TARGET_KERNEL_CONFIG := Infinix-X6833B_defconfig
-TARGET_KERNEL_SOURCE := kernel/infinix/Infinix-X6833B
+TARGET_KERNEL_CONFIG := X6833B_defconfig
+TARGET_KERNEL_SOURCE := kernel/infinix/X6833B
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
