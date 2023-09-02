abca82f4e02ab49b8d1d3c35bc76351e579f71d4
Shivelight <shivelight@tuta.io>
2023-09-02 09:43:19 +0700

    Commit forgotten changes

--- device.mk
+++ device.mk
@@ -7,12 +7,19 @@
 
 LOCAL_PATH := device/infinix/X6833B
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
