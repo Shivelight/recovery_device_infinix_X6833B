42a42a5ee94bf50394728de0ac1a603abcef7f4c
Shivelight <shivelight@tuta.io>
2023-09-04 15:34:01 +0700

    Inherit gsi_keys and emulated_storage

--- twrp_X6833B.mk
+++ twrp_X6833B.mk
@@ -9,6 +9,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
