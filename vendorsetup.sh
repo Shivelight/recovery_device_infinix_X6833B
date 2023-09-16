822287b3e86166576e5927f851eae96de4b87e54
Shivelight <shivelight@tuta.io>
2023-09-16 20:40:54 +0700

    Prettify a bit

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,15 +10,15 @@ cd ../../
 if [ -f "$TFILE" ];then
     echo "haptics path patched already, skipping"
 elif [ $REVERSE -eq 0 ]; then
-	echo "$TFILE is not found but git is able to reverse haptics path patch, assuming it's already patched, skipping"
+    echo "$TFILE is not found but git is able to reverse haptics path patch, assuming it's already patched, skipping"
 else
     cd bootable/recovery
     git apply ../../device/infinix/X6833B/patches/0001-Change-haptics-activation-file-path.patch || RET=$?
     cd ../../
     if [ $RET -ne 0 ];then
-	echo "ERROR: minuitwrp/events.cpp could not be patched! Vibration in TWRP will not work."
+        echo "ERROR: minuitwrp/events.cpp could not be patched! Vibration in TWRP will not work."
     else
-	echo "OK: minuitwrp/events.cpp patched"
-	touch $TFILE
+        echo "OK: minuitwrp/events.cpp patched"
+        touch $TFILE
     fi
 fi
